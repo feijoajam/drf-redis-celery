d50f8ee95237c1555d7cf7e34148742e83c51632
A-Girl-Has-No-Name <modasmic@mail.ru>
2023-01-16 22:23:09 +0300

    + plan, nested serialiser used. also with "prefetch"

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, TariffPlan
+
+
+class TariffPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TariffPlan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = TariffPlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
