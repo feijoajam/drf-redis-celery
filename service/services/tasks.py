634218d34d9ede009312394a1b3c89bca8611a72
A-Girl-Has-No-Name <modasmic@mail.ru>
2023-01-26 17:28:27 +0300

    fixed problems with parallel tasks

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,12 +1,16 @@
+import time
+
 from celery import shared_task
 from celery_singleton import Singleton
+from django.db import transaction
 
 
 @shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
 
-    subscription = Subscription.objects.get(id=subscription_id)
-    new_price = subscription.service.service_price - (subscription.service.service_price * (subscription.plan.discount_percent / 100.00))
-    subscription.price = new_price
-    subscription.save()
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+        new_price = subscription.service.service_price - (subscription.service.service_price * (subscription.plan.discount_percent / 100.00))
+        subscription.price = new_price
+        subscription.save()
