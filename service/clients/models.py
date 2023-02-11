cd9c0f348c5696e018f6dc534d329d1204b1a032
A-Girl-Has-No-Name <modasmic@mail.ru>
2023-02-11 20:01:30 +0300

    added index on client

--- service/clients/models.py
+++ service/clients/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Client (models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-    company_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, db_index=True)
     company_address = models.CharField(max_length=100)
 
     def __str__(self):
